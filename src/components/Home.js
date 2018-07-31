37ecd37b283a0dd1b14a60f20d27331e124be64c
litufu <285212818@qq.com>
2018-07-31 18:50:01 +0800

    增加事件监听和相应，通过state改变状态值

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class Home extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      age:props.age
+    }
+    this.onMakeOlder = this.onMakeOlder.bind(this)
+  }
+
+  onMakeOlder() {
+    this.setState(prevState=>({
+      age: prevState.age+3
+    }));
+  }
+
   render() {
     return (
       <div className="App">
         <div>your name is {this.props.name}</div>
-        <div>your age is {this.props.age}</div>
+        <div>your age is {this.state.age}</div>
+        <button  onClick={this.onMakeOlder}>
+          older 3 ages
+        </button>
         <div>
           hobbies:
           <ul>
